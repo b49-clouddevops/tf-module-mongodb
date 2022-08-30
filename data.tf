68b187214983f76c6ed27ecb6f0cb7bc74f10692
mvpc-11232 <mvp2612@hotmail.com>
2022-08-30 08:33:34 +0530

    Auto Push Commit

--- data.tf
+++ data.tf
@@ -1,5 +1,5 @@
-
-data "terraform_remote_state" "remote" {
+# Using this remote data source, we are fetching the outputs from remote state file. in this case VPC Statefile
+data "terraform_remote_state" "vpc" {
   backend = "s3"
   config {
     bucket = "b49-rf-remote-state-bucket"
