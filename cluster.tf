53890d5886f7827d7b0f415b35bc00f8ac4bf2ee
mvpc-11232 <mvp2612@hotmail.com>
2022-08-30 08:04:03 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -4,6 +4,6 @@ resource "aws_docdb_cluster" "docdb" {
   engine                  = "docdb"
   master_username         = "admin1"
   master_password         = "roboshop1"
-  backup_retention_period = 5
-  skip_final_snapshot     = true
+  backup_retention_period = 
+  skip_final_snapshot     = true   # terraform destroy won't ask you that do you need a backup before deletion or not
 }
