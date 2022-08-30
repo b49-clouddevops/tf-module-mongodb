b9606068219aa36627455205454ae95e03d5cba7
mvpc-11232 <mvp2612@hotmail.com>
2022-08-30 08:05:50 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -5,4 +5,16 @@ resource "aws_docdb_cluster" "docdb" {
   master_username         = "admin1"
   master_password         = "roboshop1"
   skip_final_snapshot     = true   # terraform destroy won't ask you that do you need a backup before deletion or not
+  db_subnet_group_name    =  ??????
+}
+
+
+# Creating Subnet Grouo 
+resource "aws_docdb_subnet_group" "default" {
+  name       = "main"
+  subnet_ids = [aws_subnet.frontend.id, aws_subnet.backend.id]
+
+  tags = {
+    Name = "My docdb subnet group"
+  }
 }
