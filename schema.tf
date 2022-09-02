3e52de7abab7abc357f5df233de5fc187ad35b9c
mvpc-11232 <mvp2612@hotmail.com>
2022-09-02 15:43:27 +0530

    Auto Push Commit

--- schema.tf
+++ schema.tf
@@ -5,7 +5,7 @@ resource "null_resource" "mongod-schema" {
   provisioner "local-exec" {
     command = <<EOF
     cd /tmp 
-    curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+    curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
     unzip -o mongodb.zip 
     cd mongodb-main 
     mongo --ssl --host roboshop-dev.cluster-ciqb6xvrbz0q.us-east-1.docdb.amazonaws.com:27017 --sslCAFile /home/centos/rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < catalogue.js
