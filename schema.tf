bcfc776d86f451730d79ee8e0f33acbf545094ca
mvpc-11232 <mvp2612@hotmail.com>
2022-09-02 15:46:13 +0530

    Auto Push Commit

--- schema.tf
+++ schema.tf
@@ -8,9 +8,10 @@ resource "null_resource" "mongod-schema" {
     curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
     unzip -o mongodb.zip 
     cd mongodb-main 
-    
     mongo --ssl --host roboshop-dev.cluster-ciqb6xvrbz0q.us-east-1.docdb.amazonaws.com:27017 --sslCAFile /home/centos/rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < catalogue.js
     mongo --ssl --host roboshop-dev.cluster-ciqb6xvrbz0q.us-east-1.docdb.amazonaws.com:27017 --sslCAFile /home/centos/rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < users.js
 EOF
   }
 }
+
+# mongo -ssl is the way to connect to docuementDB using the .pem key. 
