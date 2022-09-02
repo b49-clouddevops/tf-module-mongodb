8e8c3e67a32527b9776a8f649067bfb7ba683b12
mvpc-11232 <mvp2612@hotmail.com>
2022-09-02 11:58:26 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -1,7 +1,7 @@
 # Created Document DB : A Managed service for MongoDB
 resource "aws_docdb_cluster" "docdb1" {
   cluster_identifier      = "roboshop-${var.ENV}"
-  engine                  = "docdb1"
+  engine                  = "docdb"
   master_username         = "admin1"
   master_password         = "roboshop1"
   skip_final_snapshot     = true   # terraform destroy won't ask you that do you need a backup before deletion or not
