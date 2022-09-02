2d80c5d408d707ec78442ad593d841990cbeeb77
mvpc-11232 <mvp2612@hotmail.com>
2022-09-02 11:22:56 +0530

    Auto Push Commit

--- schema.tf
+++ schema.tf
@@ -1,14 +1,14 @@
-# Injecting the Schema
-resource "null_resource" "mongod-schema" {
+# # Injecting the Schema
+# resource "null_resource" "mongod-schema" {
   
-  depends_on  =  [aws_docdb_cluster.docdb]
-  provisioner "local-exec" {
-    command = <<EOF
-    cd /tmp 
-    curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
-    unzip -o mysql.zip 
-    cd mysql-main 
-    mysql -h ${aws_db_instance.mysql.address}  -uadmin1 -pRoboShop1 <shipping.sql
-EOF
-  }
-}
+#   depends_on  =  [aws_docdb_cluster.docdb]
+#   provisioner "local-exec" {
+#     command = <<EOF
+#     cd /tmp 
+#     curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
+#     unzip -o mysql.zip 
+#     cd mysql-main 
+#     mysql -h ${aws_db_instance.mysql.address}  -uadmin1 -pRoboShop1 <shipping.sql
+# EOF
+#   }
+# }
