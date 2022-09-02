60c46631f0e32f7853f5dbe4e7dc546f547a16e6
mvpc-11232 <mvp2612@hotmail.com>
2022-09-02 11:50:04 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -12,7 +12,7 @@ resource "aws_docdb_cluster" "docdb1" {
 
 # Creating Subnet Grouo 
 resource "aws_docdb_subnet_group" "docdb1" {
-  identifier = "${local.TAG_PREFIX}-docdb-instance"
+  identifier = "roboshop-docdb1-${var.ENV}"
   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
 
   tags = {
