7c254c59d25f8a0afc26d3a908ccc12c008be6f7
mvpc-11232 <mvp2612@hotmail.com>
2022-09-03 08:12:35 +0530

    Auto Push Commit

--- route53.tf
+++ route53.tf
@@ -1,4 +1,5 @@
 resource "aws_route53_record" "www" {
+    # I'd need the zone details, which we have given in the vpc module, so we need to fetch the zone id from there. We are can read remote state from their outputs
   zone_id = aws_route53_zone.primary.zone_id
   name    = "www.example.com"
   type    = "A"
