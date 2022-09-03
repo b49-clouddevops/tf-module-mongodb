be7a8daa0f9caa510450063506cd34f0d3a9debc
mvpc-11232 <mvp2612@hotmail.com>
2022-09-03 08:19:21 +0530

    Auto Push Commit

--- route53.tf
+++ route53.tf
@@ -1,8 +1,8 @@
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "mongo-record" {
     # I'd need the zone details, which we have given in the vpc module, so we need to fetch the zone id from there. We are can read remote state from their outputs
   zone_id = data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ID
   name    = "mongodb-${var.ENV}.${data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ZONE}"
   type    = "CNAME"
   ttl     = 660
-  records = [aws]
+  records = [aws_docdb_cluster.main.address]
 }
