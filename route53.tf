fd52a95074e0dbe9d2e417467d7a3dce8093109b
mvpc-11232 <mvp2612@hotmail.com>
2022-09-03 08:15:59 +0530

    Auto Push Commit

--- route53.tf
+++ route53.tf
@@ -1,8 +1,8 @@
 resource "aws_route53_record" "www" {
     # I'd need the zone details, which we have given in the vpc module, so we need to fetch the zone id from there. We are can read remote state from their outputs
-  zone_id = aws_route53_zone.primary.zone_id
+  zone_id = data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PUBLIC_ZONE
   name    = "www.example.com"
-  type    = "A"
-  ttl     = 300
+  type    = "CNAME"
+  ttl     = 660
   records = [aws_eip.lb.public_ip]
 }
