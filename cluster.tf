6625e7397930eefcc0403373a3d359ae77c38aa4
mvpc-11232 <mvp2612@hotmail.com>
2022-09-04 08:31:05 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -2,8 +2,10 @@
 resource "aws_docdb_cluster" "main" {
   cluster_identifier              = "roboshop-${var.ENV}"
   engine                          = "docdb"
-  master_username                 = jsondecode(aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_USERNAME"]
-  master_password                 = jsondecode(aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_PASSWORD"]
+  # master_username                 = jsondecode(aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_USERNAME"]
+  # master_password                 = jsondecode(aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_PASSWORD"]
+  master_username                 = admin1 
+  master_password                 = roboshop1
   skip_final_snapshot             = true
   db_subnet_group_name            = aws_docdb_subnet_group.docdb.name
   vpc_security_group_ids          = [aws_security_group.allow_mongodb.id]
